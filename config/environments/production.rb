1537874a74d1b82f90dffd477600226255b8baea
Tim Diggins <tim@red56.co.uk>
2014-04-01 15:44:42 +0100

    adding in sendgrid settings for production on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ LofiLions::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: ENV['CANONICAL_HOST'] }
+
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+
 end
