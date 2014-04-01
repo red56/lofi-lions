18837c32d01a50f2778ff3cede9a8415a838e6df
Tim Diggins <tim@red56.co.uk>
2014-04-01 23:54:03 +0100

    displays and edits plural forms of master texts

--- app/controllers/master_texts_controller.rb
+++ app/controllers/master_texts_controller.rb
@@ -69,6 +69,6 @@ class MasterTextsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def master_text_params
-      params.require(:master_text).permit(:key, :many, :text, :comment)
+      params.require(:master_text).permit(:key, :many, :text, :comment, :pluralizable)
     end
 end
--- spec/features/master_text_pages_spec.rb
+++ spec/features/master_text_pages_spec.rb
@@ -20,6 +20,15 @@ describe 'Master Text Pages' do
       MasterText.stub(all: texts)
       visit master_texts_path
     end
+    context "with plural forms" do
+      it "can list one" do
+        texts = build_stubbed_list(:master_text, 1, pluralizable: true, one: 'one-one', many: 'many-many-many')
+        MasterText.stub(all: texts)
+        visit master_texts_path
+        page.should have_content('many-many-many')
+        page.should have_content('one-one')
+      end
+    end
     it "links to new" do
       visit master_texts_path
       page.should have_link_to(new_master_text_path)
@@ -53,8 +62,8 @@ describe 'Master Text Pages' do
     end
   end
   describe "edit" do
-    let(:master_text){create(:master_text)}
-    before{ visit edit_master_text_path(master_text)}
+    let(:master_text) { create(:master_text) }
+    before { visit edit_master_text_path(master_text) }
     it "displays" do
       visit new_master_text_path
     end
@@ -65,6 +74,29 @@ describe 'Master Text Pages' do
       click_on "Save"
       page.should_not have_css("form.master_text")
     end
+    it "can change pluralizable" do
+      LocalizedTextEnforcer.any_instance.should_receive(:master_text_changed).with(master_text)
+      page.should have_css("form.master_text")
+      expect {
+        page.check('plural')
+        click_on "Save"
+      }.to change { master_text.reload.pluralizable }
+      page.should_not have_css("form.master_text")
+
+    end
+
+    context "when pluralizable" do
+      let(:master_text) { create(:master_text, pluralizable: true) }
+      it "allows me to change one and many" do
+        fill_in "master_text_one", with: "My one one"
+        fill_in "master_text_many", with: "My many many"
+        expect {
+          click_on "Save"
+        }.to change { [master_text.reload.one, master_text.many] }
+        page.should_not have_css("form.master_text")
+      end
+    end
+
     it "displays errors" do
       LocalizedTextEnforcer.any_instance.should_not_receive(:master_text_changed)
       page.should have_css("form.master_text")
