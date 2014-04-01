8de7716599a94f281a867c52923ddb6dffa03636
Tim Diggins <tim@red56.co.uk>
2014-04-02 00:56:09 +0100

    removed debugging line

--- spec/controllers/master_texts_controller_spec.rb
+++ spec/controllers/master_texts_controller_spec.rb
@@ -20,7 +20,6 @@ describe MasterTextsController do
 
     describe "with valid params" do
       it "creates a new MasterText" do
-        puts attributes_for(:master_text)
         expect {
           post :create, {:master_text => attributes_for(:master_text)}
         }.to change(MasterText, :count).by(1)
