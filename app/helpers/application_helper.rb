492025e194d54ed8988d7778f4592e97f94a9ca6
Tim Diggins <tim@red56.co.uk>
2014-07-03 15:49:07 +0100

    simplifying view when not logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def active_section(section)
     @section == section ? {class:"active"} : {}
   end
+
+  def version_details
+    "#{deployment_env} v#{version_number} #{deployment_commit_hash}"
+  end
+
+  def deployment_env
+    Rails.env unless Rails.env.production?
+  end
+
+  def deployment_commit_hash
+    (ENV['COMMIT_HASH'] ?  "##{ENV['COMMIT_HASH']}" : '(dev)') unless Rails.env.production?
+  end
+
+  def version_number
+    VERSION
+  end
 end
