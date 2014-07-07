16bc4ead431c0227e6cde757da3ffcc28abcda9e
Tim Diggins <tim@red56.co.uk>
2014-07-07 23:53:25 +0100

    improving ordering of views

--- app/models/view.rb
+++ app/models/view.rb
@@ -5,6 +5,8 @@ class View < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
+  default_scope order('name asc')
+  
   def keys
     @keys ||= master_texts.collect{|mt| mt.key}.join("\n")
   end
