fae5f86be84d1ad9ae2e7657560e2dd4e61cdcf8
Damilola Odelola <damzcodes@gmail.com>
2015-01-20 16:37:16 +0000

    Add failing spinach tests for new user -wip

---
+++ features/steps/new_user.rb
@@ -0,0 +1,29 @@
+class Spinach::Features::NewUser < Spinach::FeatureSteps
+  step 'I am logged in as an administrator' do
+    pending 'step not implemented'
+  end
+
+  step 'I create a new user "someone@example.com"' do
+    pending 'step not implemented'
+  end
+
+  step 'an email with a password reset link should be added to the delayed jobs' do
+    pending 'step not implemented'
+  end
+
+  step 'when the email is sent' do
+    pending 'step not implemented'
+  end
+
+  step 'in a new browser session' do
+    pending 'step not implemented'
+  end
+
+  step 'they should be able to see the email' do
+    pending 'step not implemented'
+  end
+
+  step 'click "password reset"' do
+    pending 'step not implemented'
+  end
+end
