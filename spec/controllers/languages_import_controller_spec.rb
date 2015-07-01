d9a636f14b26a006a3d6768a77ec68627bf9e4bf
EllaNancyFay <ella@red56.uk>
2015-07-01 14:22:28 +0100

    yaml import works for localizations (languages other then english)

--- spec/controllers/languages_import_controller_spec.rb
+++ spec/controllers/languages_import_controller_spec.rb
@@ -92,6 +92,46 @@ describe LanguagesImportController, :type => :controller do
       end
     end
   end
+
+  describe "Yaml" do
+    let(:file_path) { "simple_strings.yml" }
+
+    context("mocked") do
+      before do
+        localizations = build_list(:localization, 3)
+      end
+
+      it "accepts a yaml upload" do
+        expect(Language).to receive(:find_by_code).with('zh').and_return(chinese)
+        expect(RailsYamlFormat::YamlFile).to receive(:parse).and_return(localizations)
+        expect(localizations).to receive(:close)
+        expect(Localization).to receive(:create_localized_texts).with(chinese, a_kind_of(Localization::Collection))
+        post :yaml, {file: file_upload, id: 'zh', format: 'json'}
+      end
+      it "should stop if it receives unknown code" do
+        expect(Language).to receive(:find_by_code).with('zh').and_return(nil)
+        expect(Localization).not_to receive(:create_localized_texts)
+        post :yaml, {file: file_upload, id: 'zh', format: 'json'}
+      end
+    end
+
+    context("full-stack") do
+      before {
+        create :language, code: 'zh'
+        ["Adding", "Almost done", "Done"].each do |key|
+          create :master_text, key: key
+        end
+      }
+
+      it "creates the expected master texts" do
+        post :yaml, {file: file_upload, id: 'zh', format: 'json'}
+        localized = LocalizedText.all.map { |mt| [mt.key, mt.other] }
+        expect(localized).to include(["Adding", "Adding..."])
+        expect(localized).to include(["Almost done", "Almost done..."])
+        expect(localized).to include(["Done", "Done!"])
+      end
+    end
+  end
 end
 
 
