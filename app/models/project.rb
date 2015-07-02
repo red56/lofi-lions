e79f25d4b1cb964c98496f7d80d7864e9486aa49
EllaNancyFay <ella@red56.uk>
2015-07-02 16:14:52 +0100

    add high-level api spec

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
 
   has_many :master_texts, inverse_of: :project, dependent: :destroy
   has_many :views, inverse_of: :project, dependent: :destroy
+  has_many :localized_texts, through: :master_texts
 
   validates :name, presence: true
 
---
+++ spec/api/high_level_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+describe "High-level API spec", type: :request do
+  let(:file_upload) { fixture_file_upload(Rails.root.join("spec/fixtures/simple_strings.yml"), 'application/octet-stream') }
+  let!(:project) { create(:project) }
+  let(:language) { create(:language) }
+
+  it "can import master_texts" do
+    expect {
+      post "/projects/#{project.id}/import/yaml", file: file_upload
+    }.to change { project.reload.master_texts.count }
+  end
+
+  it "can import languages" do
+    pending
+    expect {
+      post "/languages/#{language.code}/import/yaml", file: file_upload
+    }.to change { project.reload.localized_texts.count }
+  end
+
+  context "with some localized texts" do
+    before {
+      mt = create(:master_text, project: project, key: 'key', other: 'there')
+      mt.localized_texts.create!(language: language, text: 'voila')
+    }
+    it "can export" do
+      get "/export/yaml/#{language.code}"
+      expect(response.status).to eq(200)
+      expect(response.body).to include("voila")
+    end
+  end
+
+end
