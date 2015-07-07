8804d3d60a134679611ff13493596457f2d39913
EllaNancyFay <ella@red56.uk>
2015-07-07 17:48:59 +0100

    add tabs to project show - wip

--- spec/features/project_pages_spec.rb
+++ spec/features/project_pages_spec.rb
@@ -7,9 +7,25 @@ describe 'Project Pages', :type => :feature do
     stubbed_login_as_admin_user
   end
 
-  it "shows project" do
+  it "shows project name" do
     visit project_path(project)
     expect(page).to have_content(project.name)
   end
 
+  describe "the tabs" do
+    it "links to master texts" do
+      visit project_path(project)
+      expect(page).to have_link_to(project_master_texts_path(project))
+    end
+
+    it "links to views" do
+      visit project_path(project)
+      expect(page).to have_link_to(project_views_path(project))
+      end
+
+    it "links to overview" do
+      visit project_path(project)
+      expect(page).to have_link_to(project_path(project))
+    end
+  end
 end
