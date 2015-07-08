e03a6e7b19979dd913f82a30140bb56167d12322
EllaNancyFay <ella@red56.uk>
2015-07-08 11:42:36 +0100

    verify partial doubles using mocks syntax expect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ RSpec.configure do |config|
     # Enable only the newer, non-monkey-patching expect syntax.
     # For more details, see:
     #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-    # mocks.syntax = :expect
+    mocks.syntax = :expect
 
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended.
