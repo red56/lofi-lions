07f3a46aaf6fbd16d12ea771f047c01abd7efaac
Tim Diggins <tim@red56.co.uk>
2015-12-18 15:34:24 +0000

    making projects editable

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,53 @@
 class ProjectsController < ApplicationController
+  before_action :find_project
+  before_action :require_administrator!, except: [:show]
 
   def show
-    @project = Project.find(params[:id])
     @active_tab = :overview
   end
 
+  def edit
+    @active_tab = :edit
+  end
+
+  def update
+    if update_project_with_languages
+      redirect_to @project
+    else
+      @active_tab = :edit
+      render :edit
+    end
+  end
+
+  protected
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+  def update_project_with_languages
+    @project.update(project_params).tap do |success|
+      if success
+        project_language_id_params.each do |id|
+          if @language = get_additional_language(id)
+            project_language = ProjectLanguage.new(project: @project, language: @language)
+            LocalizedTextEnforcer::ProjectLanguageCreator.new(project_language).save or fail ("failed to create ProjectLanguage")
+          end
+        end
+      end
+    end
+  end
+
+  def project_language_id_params
+    params[:project][:language_ids]
+  end
+
+  def get_additional_language(language_id)
+    return nil unless language_id.present? && @language = Language.where(id: language_id).first
+    return nil if @project.languages.include?(@language)
+    @language
+  end
 end
--- spec/features/project_pages_spec.rb
+++ spec/features/project_pages_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 describe 'Project Pages', :type => :feature do
   let!(:project) { create(:project, name: "steve") }
-  let!(:project_language) {create(:project_language, language_id: language.id, project_id: project.id)}
-  let(:language) {create(:language, name: "French")}
+  let!(:project_language) { create(:project_language, language_id: language.id, project_id: project.id) }
+  let(:language) { create(:language, name: "French") }
 
-  before do
-    stubbed_login_as_admin_user
-  end
+  let(:do_logging_in) { stubbed_login_as_admin_user }
+  before { do_logging_in }
 
   it "shows project name" do
     visit project_path(project)
@@ -29,11 +28,38 @@ describe 'Project Pages', :type => :feature do
     it "links to views" do
       visit project_path(project)
       expect(page).to have_link_to(project_views_path(project))
-      end
+    end
 
     it "links to overview" do
       visit project_path(project)
       expect(page).to have_link_to(project_path(project))
     end
   end
+
+  describe "edit" do
+    let!(:other_language) { create(:language, name: "Spanish") }
+
+    it "can edit name" do
+      visit edit_project_path(project)
+      fill_in "Name", with: "What you like"
+      click_on "Save"
+      expect(project.reload.name).to eq "What you like"
+    end
+    it "can add new project languages" do
+      visit edit_project_path(project)
+      check("project_language_ids_#{other_language.id}")
+      click_on "Save"
+      expect(project.reload.languages).to include(other_language)
+    end
+
+    context "with non admin user" do
+      let(:do_logging_in) { stubbed_login_as_user }
+
+      it "won't work" do
+        expect {visit edit_project_path(project)}.to raise_error /Not Found/
+      end
+    end
+  end
+
+
 end
