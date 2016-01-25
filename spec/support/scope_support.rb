c99fd6d6c2dbeb838298079f53d3a833299a71ca
Tim Diggins <tim@red56.co.uk>
2016-01-25 17:31:05 +0000

    restyling and reordering layouts

--- spec/support/scope_support.rb
+++ spec/support/scope_support.rb
@@ -1,5 +1,22 @@
-# This allows us to let the passed in scope behave like a scope (accept .order and .includes and return itself)
 def allow_to_behave_like_scope(scope)
-  scope.define_singleton_method(:order) { |*args| scope }
-  scope.define_singleton_method(:includes) { |*args| scope }
+  like_a_scope(scope)
+end
+
+# allows an array to behave like a scope
+def like_a_scope(array, type: nil)
+  array.tap do |scope|
+    scope.define_singleton_method(:includes){|*args| scope}
+    scope.define_singleton_method(:where){|*args| scope}
+    scope.define_singleton_method(:limit){|*args| scope}
+    scope.define_singleton_method(:order){|*args| scope}
+    scope.define_singleton_method(:reorder){|*args| scope}
+    scope.define_singleton_method(:select){|*args| scope}
+    scope.define_singleton_method(:per){|*args| scope} #kaminari
+    scope.define_singleton_method(:page){|*args| scope} #kaminari
+    case type
+    when nil
+    else
+      fail "case for #{type} not found"
+    end
+  end
 end
