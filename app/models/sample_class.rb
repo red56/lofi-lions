c98d0376735bf544bb35c5f97f2ee242e408cf65
Tim Diggins <tim@red56.uk>
2018-06-30 08:42:54 +0100

    removing fixed issues. (as of RubyMine 2018.2 EAP Build #RM-182.3458.16, built on June 27, 2018)

--- app/models/sample_class.rb
+++ app/models/sample_class.rb
@@ -20,21 +20,4 @@ class SampleClass
     # Seems similar to the behaviour in https://youtrack.jetbrains.com/issue/RUBY-19385#comment=27-2836602
   end
 
-  def for_strange_error
-    raise ExternalLibraries::Errors::ApiError.new("library_ident", "method", "path", "data", "response_code", "body")
-    # above line is marked as error in RubyMine "Found extra argument(s). Required <= 1: msg=… less... (⌘F1) "
-    # ctrl-clicking `new` takes you to Exception.new
-    # code introspection seems to be based on Exception.new, not to ExternalLibraries::Errors::ApiError#initialize
-  end
-
-  def non_navigable
-    @api_client ||= ApiClient.new("flong")
-    File.open(url) do |tempfile|
-      file_type = correct_mimetype(entry.file_content_type)
-      api_client.start(file_type)
-      # command click on "start" for code navigation
-      # I get no navigation suggestions
-    end
-  end
-
 end
