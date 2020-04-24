6d9afa634ed334d65dcd2f9e91543fb7cec211d4
Tim Diggins <tim@red56.uk>
2020-04-24 07:10:37 +0100

    update devise (security advisory)

--- spec/support/authentication_support.rb
+++ spec/support/authentication_support.rb
@@ -26,5 +26,5 @@ def ensure_user
 end
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
