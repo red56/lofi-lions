733bd303bcf49b5d6712e04dc67e0c79d0f7f241
Tim Diggins <tim@red56.uk>
2020-08-25 16:09:29 +0100

    Improve handling of rails yml. (#46)
    * input nested keys to key separated by "/"
    * export key separated by "/" to nested keys
    * output string rather than symbol top level language codes

--- lib/rails_yaml_format/exporter.rb
+++ lib/rails_yaml_format/exporter.rb
@@ -10,11 +10,24 @@ module RailsYamlFormat
 
     def body_for(localized_texts)
       top_level_hash = {}
+      # lower_level_hash = Hash[localized_texts.map {|text|[text.key, text.other_export] }]
+      lower_level_hash = {}
+      localized_texts.each do |text|
+        add_to_hash(lower_level_hash, keys: text.key.split("/"), value: text.other_export)
+        # lower_level_hash[text.key] = text.other_export
+      end
 
-      lower_level_hash = Hash[localized_texts.map {|text|[text.key, text.other_export] }]
-
-      top_level_hash[@language.code.to_sym] = lower_level_hash
+      top_level_hash[@language.code] = lower_level_hash
       top_level_hash.to_yaml
+    end
+
+    def add_to_hash(hash, keys: , value: )
+      key = keys.shift
+      if keys.empty?
+        hash[key] = value
+      else
+        add_to_hash(hash[key] ||= {}, keys: keys, value: value)
+      end
     end
   end
 end
--- spec/lib/exporter_spec.rb
+++ spec/lib/exporter_spec.rb
@@ -30,12 +30,12 @@ end
 
 
 describe "Exporters" do
-  let(:project) {build_stubbed(:project)}
+  let(:project) { build_stubbed(:project) }
   let(:language) { build_stubbed(:language, code: 'la') }
   let(:project_language) { build_stubbed(:project_language, project: project, language: language) }
-  let(:master_texts){ build_stubbed_list(:master_text, 3, project: project)}
-  let(:localized_texts) { master_texts.map{|mt| build_stubbed(:stubbed_localized_text, master_text: mt,
-      project_language: project_language)} }
+  let(:master_texts) { build_stubbed_list(:master_text, 3, project: project) }
+  let(:localized_texts) { master_texts.map { |mt| build_stubbed(:stubbed_localized_text, master_text: mt,
+    project_language: project_language) } }
   before {
     allow(project_language).to receive(:localized_texts_with_fallback).and_return(localized_texts)
     allow(ProjectLanguage).to receive(:where).with(project_id: project.id, language_id: language.id).and_return([project_language])
@@ -56,56 +56,82 @@ describe "Exporters" do
     it_behaves_like "a BaseExporter" do
       let(:platform) { :yaml }
     end
+    let(:export) { RailsYamlFormat::Exporter.new(language, project) }
+    let(:body) { export.body }
 
     describe "body" do
-      subject { RailsYamlFormat::Exporter.new(language, project).body }
+      subject { body }
+
+      it "the file should start with expected beginning" do
+        expect(subject).to be_a String
+        lines = subject.split("\n")
+        expect(lines[0]).to eq("---")
+        expect(lines[1]).to eq("la:")
+      end
 
       it "should be valid yaml that has top level hash only containing the language code" do
         parsed = YAML.load(subject)
         expect(parsed).to be_a Hash
-        expect(parsed).to have_key(language.code.to_sym)
+        expect(parsed).to have_key(language.code)
       end
+
 
       context "with a different language" do
         let(:language) { build_stubbed(:language, code: 'fr') }
         it "has different language code as top level key" do
-          expect(YAML.load(subject)).to have_key(:fr)
+          expect(YAML.load(subject)).to have_key("fr")
         end
       end
+    end
 
-      describe "the lower level hash" do
-        subject { YAML.load(super())[language.code.to_sym] }
+    describe "the lower level hash" do
+      subject { YAML.load(body)[language.code] }
+
+      it "should have relevant number of keys" do
+        expect(subject).to be_a Hash
+        expect(subject.keys.count).to eq localized_texts.length
+      end
+      it "should have relevant keys which should be strings" do
+        localized_texts.each do |text|
+          expect(subject.keys).to include(text.key.to_s)
+        end
+      end
+      it "should have relevant values which should be strings" do
+        localized_texts.each do |text|
+          expect(subject.values).to include(text.other_export.to_s)
+        end
+      end
+      context "with only one key" do
+        let(:localized_texts) { [text] }
+        let(:text) { build_stubbed(:localized_text) }
 
         it "should have relevant number of keys" do
           expect(subject).to be_a Hash
-          expect(subject.keys.count).to eq localized_texts.length
+          expect(subject.keys.count).to eq 1
         end
-        it "should have relevant keys which should be strings" do
-          localized_texts.each do |text|
-            expect(subject.keys).to include(text.key.to_s)
-          end
+        it "should have relevant key" do
+          expect(subject.keys).to include(text.key)
         end
-        it "should have relevant values which should be strings" do
-          localized_texts.each do |text|
-            expect(subject.values).to include(text.other_export.to_s)
-          end
-        end
-        context "with only one key" do
-          let(:localized_texts) { [text] }
-          let(:text) { build_stubbed(:localized_text) }
-
-          it "should have relevant number of keys" do
-            expect(subject).to be_a Hash
-            expect(subject.keys.count).to eq 1
-          end
-          it "should have relevant key" do
-            expect(subject.keys).to include(text.key)
-          end
-          it "should have relevant value" do
-            expect(subject.values).to include(text.other_export.to_s)
-          end
+        it "should have relevant value" do
+          expect(subject.values).to include(text.other_export.to_s)
         end
       end
+    end
+
+    describe "with an (encoded) nested hash" do
+      let(:master_text1) { build_stubbed(:master_text, project: project, key: "activerecord/attributes/user/email") }
+      let(:master_text2) { build_stubbed(:master_text, project: project, key: "activerecord/attributes/user/password") }
+      let(:master_text3) { build_stubbed(:master_text, project: project, key: "activerecord/attributes/project/name") }
+      let(:master_texts) { [master_text1, master_text2, master_text3] }
+      let(:localized_texts) { master_texts.map { |mt| build_stubbed(:stubbed_localized_text, master_text: mt,
+        project_language: project_language) } }
+      subject { YAML.load(body)[language.code] }
+      it "produces actual nested output" do
+        expect(subject.keys).to contain_exactly("activerecord")
+        expect(subject["activerecord"]["attributes"].keys).to contain_exactly("user", "project")
+        expect(subject["activerecord"]["attributes"]["user"].keys).to contain_exactly("email", "password")
+      end
+
     end
   end
 end
