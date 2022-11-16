76f67a0a38dbfbda550825b3965278cca70bfe45
Tim Diggins <tim@red56.uk>
2022-11-16 17:21:31 +0000

    add strip_bullets

--- app/models/concerns/textable.rb
+++ app/models/concerns/textable.rb
@@ -6,4 +6,11 @@ module Textable
 
     text.strip.split("\n").map { |t| t.strip.presence }.compact
   end
+
+  # strips bullets from text and normalizes returns to double returns
+  def strip_bullets
+    raise "strip_bullets can't deal with pluralizable" if pluralizable
+
+    self.text = non_blank_lines.map { |line| line.sub(/^\s*[*] /, "") }.join("\n\n")
+  end
 end
--- spec/support/textable_shared.rb
+++ spec/support/textable_shared.rb
@@ -17,5 +17,37 @@ RSpec.configure do
         expect(build(factory_name, text: "one\ntwo\nthree\n").non_blank_lines).to eq(["one", "two", "three"])
       end
     end
+
+    describe "#strip_bullets" do
+      it "has a noop" do
+        textable = build(factory_name, text: "one")
+        expect { textable.strip_bullets }.not_to change { textable.text }
+      end
+
+      it "strips one" do
+        textable = build(factory_name, text: "* one")
+        expect { textable.strip_bullets }.to change { textable.text }.to "one"
+      end
+
+      it "strips one with noops" do
+        textable = build(factory_name, text: "one\n\n* two\n\nthree")
+        expect { textable.strip_bullets }.to change { textable.text }.to "one\n\ntwo\n\nthree"
+      end
+
+      it "strips three" do
+        textable = build(factory_name, text: "* one\n* two\n* three ")
+        expect { textable.strip_bullets }.to change { textable.text }.to "one\n\ntwo\n\nthree"
+      end
+
+      it "strips one with spaces" do
+        textable = build(factory_name, text: "\n\n* one\n\n\t ")
+        expect { textable.strip_bullets }.to change { textable.text }.to "one"
+      end
+
+      it "strips with windows" do
+        textable = build(factory_name, text: "\r\n* one\r\n* two\r\n* three\r\n")
+        expect { textable.strip_bullets }.to change { textable.text }.to "one\n\ntwo\n\nthree"
+      end
+    end
   end
 end
